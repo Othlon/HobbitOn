9bda5235d0e80e042ca658c43a1be750901c3b39
Othlon <othlon@gmail.com>
2015-09-15 22:33:43 +1000

    more basics in

--- src/main/java/othlon/hobbiton/HobbitOn.java
+++ src/main/java/othlon/hobbiton/HobbitOn.java
@@ -1,7 +1,48 @@
 package othlon.hobbiton;
 
-/**
- * Created by jen on 12/09/15.
- */
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.creativetab.CreativeTabs;
+import othlon.hobbiton.proxie.CommonProxy;
+
+
+@Mod(modid = "hobbiton", name = "HobbitOn", version = HobbitOn.VERSION)
+
+
 public class HobbitOn {
+
+
+        public static final String VERSION = "1.4.2";
+
+        @Mod.Instance
+        public static HobbitOn instance;
+
+        public static CreativeTabs tabHobbit;
+
+        // public static CommonHandler handler = new CommonHandler();
+
+        @SidedProxy(clientSide = "othlon.hobbiton.proxie.ClientProxy", serverSide = "othlon.hobbiton.proxie.CommonProxy")
+        public static CommonProxy proxy;
+
+
+        @Mod.EventHandler
+        public void preInit(FMLPreInitializationEvent event) {
+
+            //tabHobbit = new CPCreativeTab(CreativeTabs.getNextID(), "CherryPigTab");
+
+
+            proxy.registerRenderers();
+
+
+
+
+        }//end pre init
+
+        @Mod.EventHandler
+        public void init(FMLInitializationEvent event){
+
+        }
 }
